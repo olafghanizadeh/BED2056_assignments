6c967dd7647cd1fc5c18eec4a62e5e7e52900102
Olaf Ghanizadeh <laffen91@gmail.com>
2020-10-05 08:32:38 +0100

    attempt to filter dates

--- 4 - UiT Timeplan Scraper/uit_scraper.R
+++ 4 - UiT Timeplan Scraper/uit_scraper.R
@@ -1,5 +1,7 @@
 library(rvest)
 library(tidyverse)
+library(lubridate)
+library(stringr)
 
 uit <- "http://timeplan.uit.no/emne_timeplan.php?sem=20h&module%5B%5D=BED-2056-1&View=list"
 
@@ -11,3 +13,15 @@ scrape <- function(url, class) {
 }
 
 uit_scraped <- scrape(uit, "table")
+
+
+# I got stuck in trying to transform the date. I wanted to pipe the uit_scraped variable, select just the uit_scraped element that contained the string "WeekdayDD.MM.YYY", apply a regex to it to remove any non-numeric chars and then use .asDate to format it as a date.
+
+# One attempt to reach the goal, but I realised it is not efficient as we can do this with pipes from dplyr
+# str_remove(uit_scraped[[1]][[1]], "\\D+")
+#
+# for (i in uit_scraped) {
+#   x <- str_remove(i[[1]][[2]], "\\D+")
+#   as.Date(x, format = "%d.%m. %Y")
+# }
+
